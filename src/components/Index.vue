f7703d3fcb3bb5628fb8386d0b27416cd3a5e3d4
“lichao” <1017020555@qq.com>
2021-08-31 15:26:35 +0800

    vuex actions

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
-    <h3>{{$store.state.count}}</h3>
-    <button v-on:click="myAdd(10)">add</button>
-    <button v-on:click="myDes(10)">des</button>
+    <div>
+      <h3>{{$store.state.count}}</h3>
+      <button v-on:click="myAdd(10)">add</button>
+      <button v-on:click="myDes(10)">des</button>
+    </div>
+    <div>异步操作</div>
+    <div>
+      <button @click="handleActionsAdd(10)">异步增加</button>
+      <button @click="handleActionsReduce(10)">异步减少</button>
+    </div>
   </div>
 </template>
 
@@ -15,6 +22,12 @@ export default {
     },
     myDes(n){
       this.$store.commit('des',n)
+    },
+    handleActionsAdd(n){
+      this.$store.dispatch('actionAdd',n);
+    },
+    handleActionsReduce(n){
+      this.$store.dispatch('actionDes',n);
     }
   }
 }
--- src/vuex/store.js
+++ src/vuex/store.js
@@ -8,15 +8,24 @@ const state = {
 }
 
 const mutations = {
-  add (state, n) {
+  add(state, n) {
     return (state.count += n)
   },
-  des (state, n) {
+  des(state, n) {
     return (state.count -= n)
   }
 }
 
+const actions = {
+  actionAdd(context, n) {
+    return context.commit('add', n)
+  },
+  actionDes({commit},n) {
+    commit('des', n)
+  }
+}
 export default new Vuex.Store({
   state,
-  mutations
+  mutations,
+  actions
 })
